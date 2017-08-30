674c5322111f6170a378b95624ade5d29b318d27
apr01break <apr011097@gmail.com>
2017-08-30 15:24:54 -0500

    Cambiadito

--- src/com/incomab/view/Incomab.java
+++ src/com/incomab/view/Incomab.java
@@ -46,6 +46,8 @@ public class Incomab extends Application {
 
     public static void main(String[] args) {
         launch(args);
+        System.out.println("Alo perron");
     }
     
 }
+//Alo perron
